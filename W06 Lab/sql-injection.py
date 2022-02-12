8b64cae04e1144642aa88281837d2a571244dde6
Devan Hoyt <38635082+devanhoyt@users.noreply.github.com>
2022-02-11 22:27:33 -0600

    Update sql-injection.py

--- W06 Lab/sql-injection.py
+++ W06 Lab/sql-injection.py
@@ -1,3 +1,9 @@
+from ast import Or
+from hmac import compare_digest
+import platform
+import re
+
+    
 # PART 01: Query Generation
 # Write a function to accept two strings (username and a password) and return a single string (SQL) representing
 # the query used to determine if a user is authenticated on a given system. The query will be similar to one
@@ -9,30 +15,10 @@
 # query function and displays the resulting query. Provide the code in the report, a sample of the output from the
 # function, and a couple sentences justifying why the test cases provide adequate coverage of the valid input.
 
-def promptUser():
-    username = ""
-    password = ""
-
-    print("Please enter username:")
-    username = input("> ")
-
-    print("Please enter password:")
-    password = input("> ")
-
-    genQuery(username,password)
-
-
-
 def genQuery(username, password):
     sqlQuery = (f"SELECT authenticate FROM passwordList WHERE name='{username}' and passwd='{password}';")
-    print(f" SQL Query: {sqlQuery}\n")
+#    print(f" SQL Query: {sqlQuery}\n")
     return sqlQuery
-
-def testValid():
-    test1 = ["This_1s_A_u5ern4me", "This_1s_A_p455w0rd"]
-
-    print(f"Test 1:\n Username: {test1[0]}\n Password: {test1[1]}")
-    genQuery(test1[0], test1[1])
 
 # PART 02: Vulnerabilities
 # Generate test cases (again, each team member should generate one test case) that demonstrate a tautology attack.
@@ -43,20 +29,145 @@ def testValid():
 # each attack must be demonstrated with a separate set of test cases, a separate function feeding the test cases to
 # the query function, and justification in the lab report.
 
+def testValid():
+    test1 = ["This_1s_A_u5ern4me", "This_1s_A_p455w0rd"]
 
+    print(f"Test 1:\n Username: {test1[0]}\n Password: {test1[1]}")
+    print(genQuery(test1[0], test1[1]))
+
+def shaunTest():
+    #tautology attack
+    #union query attack
+    #additional statement attack
+    #comment attack
+    return 1
+
+def collinTest():
+    # tautology attack 
+    username = "collinbrown32"
+    password = "anything or x=x"
+    weakMitigation(username, password)
+    # union query attack 
+    username = "collinbrown32"
+    password = "UNION DROP TABLE table1;"
+    weakMitigation(username, password)
+    # additional statement attack 
+    username = "collinbrown32;DROP TABLE table1;"
+    password = "trt334"
+    weakMitigation(username, password)
+    # comment attack 
+    username = "collinbrown32#"
+    password = "DROP TABLE table1;"
+    weakMitigation(username, password)
+    return 1 
+def ivanroTest():
+    #tautology attack
+    #union query attack
+    #additional statement attack
+    #comment attack
+    return 1
+
+def steveTest():
+    #tautology attack
+    #union query attack
+    #additional statement attack
+    #comment attack
+    return 1
+
+def camilaTest():
+    #tautology attack
+    #union query attack
+    #additional statement attack
+    #comment attack
+    return 1
+
+def devanTest():
+    #tautology
+    username = "Root"
+    password = 'anything or x = x' #should have parentheses around both x's and one after anything and not before anything
+    weakMitigation(username, password)
+    strongMitigation(username, password)
+    # union query attack 
+    username = ' UNION SELECT * FROM emp_details # '
+    password = "a"
+    weakMitigation(username, password)
+    strongMitigation(username, password)
+    # additional statement attack 
+    username = "";import os; dir = '/etc/passwd'; shutil.rmtree(dir)
+    password = "justdoit"
+    weakMitigation(username, password)
+    strongMitigation(username, password)
+    # comment attack 
+    username = "admin#"
+    password = "a"
+    weakMitigation(username, password)
+    strongMitigation(username, password)
+    return 1
 
 # PART 03: Weak Mitigation
 # Create a function to provide a weak mitigation against all four attacks. This function accepts the input as a parameter
 # (or two!) and returns the sanitized input. In the lab report, provide the code, the output of the various test cases,
 # and justification that the code represents a weak mitigation to the four attack types.
 
+def weakMitigation(userName, userPassword):
+    forbidden = ["OR", "or", "AND", "and", "=", "UNION", "union", ";", "/", "#", "@", "$", "~"] #List of dangerous characters
+    for i in forbidden:
+        if(i in userName or i in userPassword):
+            print(f"Invalid input from user {userName}, use of {i} not permitted.") #Loops through user input, checking for the forbidden characters. If found, prints "Invalid"
 
+    print(genQuery(userName, userPassword))
 
 # PART 04: Strong Mitigation
-#Create a function to provide a strong mitigation against all command injection attacks. Provide the code, output showing
+# Create a function to provide a strong mitigation against all command injection attacks. Provide the code, output showing
 # that the valid test cases still work, output showing that all four malicious inputs are mitigated, and a justification
 # the approach works the way one would expect.
 
+def strongMitigation(userName, userPassword):
+    #tautology attack
+    #union query attack
+    #additional statement attack
+    #comment attack
+    if not re.match("^[a-z]*$", userName):
+        print ("Error! Only letters a-z allowed!")
+        sys.exit()
+    elif len(userName) > 10:
+        print ("Error! Only 15 characters allowed!")
+        sys.exit()
+        
+    if not re.match("^[a-z]*$", userPassword):
+        print ("Error! Only letters a-z allowed!")
+        sys.exit()
+    elif len(userPassword) > 10:
+        print ("Error! Only 15 characters allowed!")
+        sys.exit()
+    print(genQuery(userName, userPassword))
+    
+def manualTest():
+    userName = getUserName()
+    userPassword = getUserPassword()
+    
+    weakMitigation(userName, userPassword)
+    strongMitigation(userName, userPassword)
+
+
+def automatedTest():
+    testValid()
+    shaunTest()
+    collinTest()
+    ivanroTest()
+    steveTest()
+    camilaTest()    
+    devanTest()
+
+def getUserName():
+    print("Please enter username: ")
+    userName = input("> ")
+    return userName
+
+def getUserPassword():
+    print("Enter Password: ")
+    userPassword = input("> ")
+    return userPassword
 
 def main():
     option = "0"
@@ -64,10 +175,10 @@ def main():
         print("Please select an option:\n Enter 1 for manual test\n Enter 2 for automated tests\n Enter 3 to end program execution")
         option = input("> ")
         if(option == '1'):
-            promptUser()
+            manualTest()
             option = '0'
         elif(option == '2'):
-            testValid()
+            automatedTest()
             option = '0'
         elif(option == '3'):
             print("SQL Injection Program Ended. Goodbye!")
